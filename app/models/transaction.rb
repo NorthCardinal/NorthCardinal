e2d74af70e67a213e681b356b54280392a24c761
FKPS <fkps@users.noreply.github.com>
2016-05-06 03:11:45 -0400

    Transaction validations and specs

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,16 @@
 class Transaction < ActiveRecord::Base
-  belongs_to :creditor_account, class_name: 'Account', foreign_key: 'creditor_account_id'
-  
-  belongs_to :debitor_account, class_name: 'Account', foreign_key: 'debitor_account_id'
+  belongs_to :creditor_account,
+    class_name: 'Account',
+    foreign_key: 'creditor_account_id'
+
+  validates :creditor_account, presence: true
+
+  belongs_to :debitor_account,
+    class_name: 'Account',
+    foreign_key: 'debitor_account_id'
+
+  validates :debitor_account, presence: true
+
+  validates :amount, numericality: { greater_than: 0.0 }
 
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   it { should belong_to :creditor_account }
   it { should belong_to :debitor_account }
+
+  it { should validate_presence_of :creditor_account }
+  it { should validate_presence_of :debitor_account }
+  it { should validate_numericality_of(:amount).is_greater_than(0.0) }
 end
